06ed427e4021ddfb07350c5b4b61b744d9c5084d
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2023-03-29 13:51:54 +0800

    vendor/template: add board and chip template code
    1. add "board_name" and "chip_name" directory for vendor board and chip
    2. add necessary c source file under chips/chip_name directory for base os,
    eg:vendor_name_start.c/irq.c/lowput.c/timeisr.c
    3. add necessary c source file under boards/board_name/src directory for base os,
    eg:vendor_name_appinit.c/boot.c/bringup.c
    4.add necessary built file, eg: Kconfig,Make.defs
    5.add nsh defconfig for baseos bringup
    6.add rename.py to generate initial version vendor project
    
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

---
+++ chips/chip_name/vendor_name_lowputc.h
@@ -0,0 +1,42 @@
+/****************************************************************************
+ * vendor/vendor_name/chip/chip_name/vendor_name_uart.h
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+#ifndef __VENDOR_VENDOR_NAME_CHIP_CHIP_NAME_VENDOR_NAME_UART_H
+#define __VENDOR_VENDOR_NAME_CHIP_CHIP_NAME_VENDOR_NAME_UART_H
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <nuttx/arch.h>
+#include <nuttx/irq.h>
+
+#include "chip.h"
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+#endif /* __VENDOR_VENDOR_NAME_CHIP_CHIP_NAME_VENDOR_NAME_UART_H */
